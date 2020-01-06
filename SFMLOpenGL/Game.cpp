ff5ca81ce4e0a626a2cbec9f450c6dd02202309a
Mark Shiels <c00242827@itcarlow.ie>
2020-01-06 10:53:08 +0000

    faces drawn, need to be vectors

--- SFMLOpenGL/Game.cpp
+++ SFMLOpenGL/Game.cpp
@@ -7,6 +7,8 @@ gpp::Vector3 v3;
 Game::Game() : window(VideoMode(800, 600), "OpenGL Cube")
 {
 	index = glGenLists(1);
+
+	
 }
 
 Game::~Game(){}
@@ -20,7 +22,7 @@ void Game::run()
 
 	while (isRunning){
 
-		cout << "Game running..." << endl;
+		//cout << "Game running..." << endl;
 
 		while (window.pollEvent(event))
 		{
@@ -37,6 +39,15 @@ void Game::run()
 
 void Game::initialize()
 {
+	Vector3f V1(1.0f, 1.0f, -5.0f);
+	Vector3f V2(-1.0f, 1.0f, -5.0f);
+	Vector3f V3(-1.0f, -1.0f, -5.0f);
+	Vector3f V4(-1.0f, -1.0f, -5.0f);
+	Vector3f V5(1.0f, 1.0f, -15.0f);
+	Vector3f V6(-1.0f, 1.0f, -15.0f);
+	Vector3f V7(-1.0f, -1.0f, -15.0f);
+	Vector3f V8(1.0f, -1.0f, -15.0f);
+
 	isRunning = true;
 
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
@@ -49,34 +60,90 @@ void Game::initialize()
 	// Creates a new Display List
 	// Initalizes and Compiled to GPU
 	// https://www.opengl.org/sdk/docs/man2/xhtml/glNewList.xml
+
+
 	glNewList(index, GL_COMPILE);
-	glBegin(GL_QUADS);
+	glBegin(GL_TRIANGLES);
 	{
 		//Front Face
-		glColor3f(0.0f, 0.0f, 1.0f);
-		glVertex3f(v3.getX(), 1.0f, -5.0f);
-		glVertex3f(-1.0f, 1.0f, -5.0f);
-		glVertex3f(-1.0f, -1.0f, -5.0f);
-		glVertex3f(1.0f, -1.0f, -5.0f);
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glVertex3f( V1.x, V1.y, V1.z);
+		glVertex3f(V2.x, V2.y, V2.z);
+		glVertex3f(V3.x, V3.y, V3.z);
+	
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glVertex3f(V1.x, V1.y, V1.z);
+		glVertex3f(V3.x, V3.y, V3.z);
+		glVertex3f(V4.x, V4.y, V4.z);
 
 		//Back Face
-		glColor3f(0.0f, 1.0f, 0.0f);
+		glColor3f(0.0f, 0.0f, 1.0f);
+		glVertex3f(V5.x, V5.y, V5.z);
+		glVertex3f(V6.x, V6.y, V6.z);
+		glVertex3f(V7.x, V7.y, V7.z);
+
+		glColor3f(0.0f, 0.0f, 1.0f);
+		glVertex3f(V5.x, V5.y, V5.z);
+		glVertex3f(V7.x, V7.y, V7.z);
+		glVertex3f(V8.x, V8.y, V8.z);
+
+		//Top face
+		glColor3f(0.0, 1.0f, 0.0f);
+		glVertex3f(V1.x, V1.y, V1.z);
+		glVertex3f(V2.x, V2.y, V2.z);
+		glVertex3f(V6.x, V6.y, V6.z);
+
+		glColor3f(0.0, 1.0f, 0.0f);
+		glVertex3f(1.0f, 1.0f, -5.0f);
 		glVertex3f(1.0f, 1.0f, -15.0f);
 		glVertex3f(-1.0f, 1.0f, -15.0f);
+	
+		//Bottom Face
+		glColor3f(0.0, 1.0f, 0.0f);
+		glVertex3f(1.0f, -1.0f, -5.0f);
+		glVertex3f(-1.0f, -1.0f, -5.0f);
 		glVertex3f(-1.0f, -1.0f, -15.0f);
+
+		glColor3f(0.0, 1.0f, 0.0f);
+		glVertex3f(1.0f, -1.0f, -5.0f);
+		glVertex3f(1.0f, -1.0f, -15.0f);
+		glVertex3f(-1.0f, -1.0f, -15.0f);
+
+		//Left face
+		glColor3f(1.0f, 1.0f, 0.0f);
+		glVertex3f(-1.0f, 1.0f, -5.0f);
+		glVertex3f(-1.0f, -1.0f, -5.0f);
+		glVertex3f(-1.0f, -1.0f, -15.0f);
+
+		glColor3f(1.0f, 1.0f, 0.0f);
+		glVertex3f(-1.0f, 1.0f, -5.0f);
+		glVertex3f(-1.0f, 1.0f, -15.0f);
+		glVertex3f(-1.0f, -1.0f, -15.0f);
+
+		//Right face
+		glColor3f(1.0f, 1.0f, 0.0f);
+		glVertex3f(1.0f, 1.0f, -5.0f);
+		glVertex3f(1.0f, -1.0f, -5.0f);
 		glVertex3f(1.0f, -1.0f, -15.0f);
 
-		//Complete the faces of the Cube
+		glColor3f(1.0f, 1.0f, 0.0f);
+		glVertex3f(1.0f, 1.0f, -5.0f);
+		glVertex3f(1.0f, 1.0f, -15.0f);
+		glVertex3f(1.0f, -1.0f, -15.0f);
 	}
 	glEnd();
 	glEndList();
+
+
+
+	
 }
 
 void Game::update()
 {
 	elapsed = clock.getElapsedTime();
 
-	if (elapsed.asSeconds() >= 1.0f)
+	if (elapsed.asMicroseconds() >= 0.00000000001f)
 	{
 		clock.restart();
 
@@ -98,18 +165,18 @@ void Game::update()
 		}
 	}
 	
-	cout << "Update up" << endl;
+	//cout << "Update up" << endl;
 }
 
 void Game::draw()
 {
-	cout << "Drawing" << endl;
+	//cout << "Drawing" << endl;
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	cout << "Drawing Cube " << endl;
+	//cout << "Drawing Cube " << endl;
 	glLoadIdentity();
-	glRotatef(rotationAngle, 0, 0, 1); // Rotates the camera on Y Axis
+	//glRotatef(rotationAngle, 0, 0, 1); // Rotates the camera on Y Axis
 
 	glCallList(1);
 
@@ -119,6 +186,6 @@ void Game::draw()
 
 void Game::unload()
 {
-	cout << "Cleaning up" << endl;
+	//cout << "Cleaning up" << endl;
 }
 
